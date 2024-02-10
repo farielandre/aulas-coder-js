f3879cbec3524169c744ecffadb373174e0f66b2
Fariel Minozzo <farielandre@gmail.com>
2024-02-10 12:49:16 -0300

    revisao aula 01

--- js/aula01.js
+++ js/aula01.js
@@ -10,7 +10,7 @@ console.log('Ano que nasci:', ano - idadeAtual);
 
 // Exercício cálculo no prompt e alert
 
-// let numero1 = prompt('Digite um numero');
-// let numero2 = prompt('Digite outro numero');
-// console.log(parseInt(numero1) * parseInt(numero2));
-// alert('O produto é:', (parseInt(numero1) * parseInt(numero2)));
+let numero1 = prompt('Digite um numero');
+let numero2 = prompt('Digite outro numero');
+console.log(parseInt(numero1) * parseInt(numero2));
+alert('O produto é:' + (parseInt(numero1) * parseInt(numero2)));
