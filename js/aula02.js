cbe6f86042f7767c1ce742abb949780592988444
Fariel Minozzo <farielandre@gmail.com>
2024-02-19 21:36:47 -0300

    Aula 02

---
+++ js/aula02.js
@@ -0,0 +1,58 @@
+// Exercicio if else
+
+// let numero = parseInt(prompt('Digite um numero de 1 a 3'));
+
+
+// if (numero === 1){
+//     console.log('numero1');
+// } else if (numero === 2) {
+//     console.log('numero2');
+// } else if (numero === 3) {
+//     console.log('numero3');
+// } else {
+//     console.log('Número incorreto, digite um numero entre 1 a 3');
+// }
+
+// Exercicio Operadores de comparação e Lógicos
+
+// let numero = parseInt(prompt('Digite um numero entre 1 e 100'));
+
+// if (numero <= 10){
+//     console.log('Numero menor ou igual a 10');
+//     console.log(numero);
+// } else if (numero >= 11 && numero < 20){
+//     console.log('Numero maior do que 10 e menor do que 20');
+//     console.log(numero);
+// } else if (numero >= 20 && numero <= 100){
+//     console.log('Numero maior ou igual a 20');
+//     console.log(numero);
+// } else {
+//     alert('Numero não está no intervalo entre 1 e 100');
+//     console.log(numero);
+// }
+
+// Exercicio 2 Operadores de comparação e Lógicos
+
+// let letra = prompt('Digite uma letra');
+
+// if (letra === ""){
+//     console.log("Valor vazio");
+// }
+// else if (letra === "a") {
+//     console.log("A letra digitada é uma vogal: " + letra)
+// }
+// else if (letra === "e") {
+//     console.log("A letra digitada é uma vogal: " + letra)
+// }
+// else if (letra === "i") {
+//     console.log("A letra digitada é uma vogal: " + letra)
+// }
+// else if (letra === "o") {
+//     console.log("A letra digitada é uma vogal: " + letra)
+// }
+// else if (letra === "u") {
+//     console.log("A letra digitada é uma vogal: " + letra)
+// }
+// else {
+//     console.log ("Você não digitou uma letra vogal")
+// }
