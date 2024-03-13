158bf943bef35533652a68418f3e798e26b2fa72
Fariel Minozzo <farielandre@gmail.com>
2024-03-13 16:57:58 -0300

    Atividade complementar 03 - correcao

--- js/atividade03.js
+++ js/atividade03.js
@@ -1,21 +1,19 @@
 // Calculadora de parcelas
-function calcParcelas() {
 
+function calcParcelas() {
     let numero1 = parseInt(prompt('Vamos saber quanto dará sua compra ao dividir em parcelas? Primeiro, digite o valor total de sua compra:'));
     let numero2 = parseInt(prompt('Agora, digite o número de parcelas que deseja:'));
 
+    if (isNaN(numero1) || isNaN(numero2)){ // Validação antes de executar o restante do código
+        console.log('Você não digitou um número. Digite um número.')
+        return; // Return para parar a execução do código caso caia na validação acima
+    }
     if (numero2 > 1) {
         for (let i = 2; i <= numero2; i++) {
             let resultado = parseFloat(numero1 / i).toFixed([2]);
             console.log(`Se você parcelar sua compra em ${i} vezes, dará R$${resultado} sem juros por parcela`)
         }
     } 
-    //// Tentei esse controle de erro abaixo e não funcionou direito. Ele ativa a operação mesmo que eu não digite nada.
-
-    // else if (isNaN(numero1) && isNaN(numero2)){
-    //     console.log('Você não digitou um número. Digite um número.')
-    // }
-
     else {
         console.log(`O valor de sua compra em 1 vez é R$${parseFloat(numero1).toFixed([2])}`)
     }
